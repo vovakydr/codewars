929dd64dc2bb72f1936bbbd7c95dca56885da26d
vovakydr <vovakydr@mail.ru>
2023-05-08 00:10:41 +0300

    finish with strings

---
+++ Basic JS/17.js
@@ -0,0 +1,12 @@
+/**
+ * \'	single quote
+ * \"	double quote
+ * \\	backslash
+ * \n	newline
+ * \t	tab
+ * \r	carriage return
+ * \b	backspace
+ * \f	form feed
+ */
+
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
---
+++ Basic JS/18.js
@@ -0,0 +1,5 @@
+const myStr1 = "This is the start. "+"This is the end."; // Change this line
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence."
+
