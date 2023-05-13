ec16444908fcd5734ae7cdb320a0f8def4b736df
vovakydr <vovakydr@mail.ru>
2023-05-14 01:12:16 +0300

    finish basic JS

---
+++ Basic JS/88.js
@@ -0,0 +1,49 @@
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    // Only change code below this line
+    var result;
+    for(let i = 0; i<contacts.length;i++){
+      if(contacts[i].firstName == name) {
+        if(contacts[i].hasOwnProperty(prop)){
+          result = contacts[i][prop];
+          break;
+        } else {
+          result = "No such property";
+          break;
+        }
+      } else {
+        result = "No such contact"
+      }
+    }
+    return result;
+    // Only change code above this line
+  }
+  
+  lookUpProfile("Akira", "likes");
